2fbc88099bf0f1172fcc0632d577349cf9eebd85
Phil Barow <pbarow@gmail.com>
2016-11-02 10:49:24 -0700

    commitng the working version of stack.js.  Moving onto queue

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
-
+  var size = 0;
+  var stackNum = 0
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size >= 1){
+      size --;
+      return storage[size];
+    }
   };
 
   someInstance.size = function() {
+  
+  return size;
   };
 
   return someInstance;
