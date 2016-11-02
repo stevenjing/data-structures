16da233a3865aede6fae0901e6e2be39c68b7369
Steven Jing <steven.jing@gmail.com>
2016-11-02 14:40:56 -0700

    Finish prototypal pattern for Stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.length = 0;
+  someInstance.front = 0;
+  someInstance.back = 0;
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
 
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+  return this.length;
+};
