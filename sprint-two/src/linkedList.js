e88576dd9042eef73922422174e35434b076d6c9
stevenjing <steven.jing@gmail.com>
2016-11-02 15:53:12 -0700

    completed the linked list exercise

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,48 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.counter = 0;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+
+  if (list.counter === 0){
+    list.head = node;
+    list.tail = node;
+  } else {
+    var temp = list.tail;
+    temp.next = node;
+    list.tail = node;
+  }
+
+  list.counter++;
+
   };
 
   list.removeHead = function() {
+
+    var formerHeadVal = list.head.value;
+    list.head = list.head.next; 
+    return formerHeadVal;
   };
 
   list.contains = function(target) {
+
+    // while next property is not == to null check the value
+    // return true if value is found
+
+    var current = list.head;
+
+    do {
+      if (current.value === target) {
+        return true;
+      }
+      //  move to the next node
+      current = current.next;
+    } while (current !== null);
+
+    return false;
+  
   };
 
   return list;
