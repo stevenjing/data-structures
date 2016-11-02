374b76089041707d87d0a4fbd04fd10fb60a2ce7
Steven Jing <steven.jing@gmail.com>
2016-11-02 12:28:27 -0700

    Complete funct and funct-shared patterns

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.length = 0;
+  someInstance.front = 0;
+  someInstance.back = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back++;
+  this.length++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.length > 0) {
+    var head = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    this.length--;
+    return head;
+  }
+};
+
+queueMethods.size = function() {
+  return this.length;
+};
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var front = 0;
+  var back = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[back] = value;
+    back++;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+      var head = storage[front];
+      delete storage[front];
+      front++;
+      return head;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
