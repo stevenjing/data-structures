38ff1d46f10c96f82d5439cd40de1535c12a0c22
stevenjing <steven.jing@gmail.com>
2016-11-03 16:18:59 -0700

    saving finished hash table functions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,30 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
+  for (let i = 0; i < this._limit; i++ ) {
+    this._storage.set(i, {});    
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  
+  bucket[k] = v; 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  delete bucket[k];   
 };
 
 
