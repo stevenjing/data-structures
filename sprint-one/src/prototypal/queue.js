127dee65874321a5e713fa5b56d0ece9d4d4d33a
stevenjing <steven.jing@gmail.com>
2016-11-03 20:34:06 -0700

    Fix bug in prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -4,6 +4,7 @@ var Queue = function() {
 
   var obj = Object.create(queueMethods);
 
+  obj.length = 0;
   obj.first = 0;
   obj.last = 0;
 
@@ -12,21 +13,23 @@ var Queue = function() {
 
 var queueMethods = {};
 
-queueMethods.enqueue = function(value){
-  	this[this.last] = value;
-  	this.last ++;
-  }; 
+queueMethods.enqueue = function(value) {
+  this[this.last] = value;
+  this.last ++;
+  this.length ++;
+}; 
 
-queueMethods.dequeue = function(){
-  	if(this.last - this.first >= 1){
-  	  this.first ++;
-  	  return this[this.first - 1];
-  	}
-  };
+queueMethods.dequeue = function() {
+  if (this.last - this.first >= 1) {
+    this.first ++;
+    this.length --;
+    return this[this.first - 1];
+  }
+};
 
-queueMethods.size = function(){
-  	return this.last - this.first;
-  };
+queueMethods.size = function() {
+  return this.last - this.first;
+};
 
 queueMethods.size = function() {
   return this.length;
