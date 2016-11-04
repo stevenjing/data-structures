275c8a2cd658ed063e3bb2468be64164242461e2
stevenjing <steven.jing@gmail.com>
2016-11-03 18:38:16 -0700

    almost finished with binary tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,84 @@
 var BinarySearchTree = function(value) {
+  
+  var obj = Object.create(BinarySearchTree.prototype);
+  
+
+  obj.value = value || null;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
 
+BinarySearchTree.prototype.insert = function (value, node) {
+
+
+  node = node || this;
+
+  if (node.value === null) {
+    node.value = value;
+  }
+
+  if (node.value < value) {
+    if (node.right !== null) {
+      node.insert(value, node.right);
+    } else {
+      node.right = BinarySearchTree(value);
+    }
+  }
+
+  if (node.value > value) {
+    if (node.left !== null) {
+      node.insert(value, node.left);
+    } else {
+      node.left = BinarySearchTree(value);
+    }
+  }
+
+};
+
+BinarySearchTree.prototype.contains = function (value, node) {
+
+  var match = false;
+  node = node || this;
+
+
+  function search (value, node) {
+
+    if (value === node.value) {
+      match = true;
+    }
+
+    if (node.left !== null) {
+      search(value, node.left);
+    }
+    
+    if (node.right !== null) {
+      search(value, node.right);
+    }
+  }
+
+  // if time come back and look at the potential recursive pattern below
+  // return (contains(left) || contains(right));
+
+  search(value, node);
+  return match;
+
+}; 
+
+BinarySearchTree.prototype.depthFirstLog = function (cb, node) {
+
+  node = node || this;
+
+  cb(node.value);
+
+  if (node.left !== null) {
+    node.depthFirstLog(cb, node.left);
+  }
+    
+  if (node.right !== null) {
+    node.depthFirstLog(cb, node.right);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
